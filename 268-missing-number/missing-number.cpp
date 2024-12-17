4d983ddc52577d85073bb1e6ae8b520dffd599fa
Ujjawal Patidar <118273885+ujjawalpatidar4@users.noreply.github.com>
2024-12-17 11:04:38 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (16.65%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,9 +4,7 @@ public:
         int n = arr.size();
         int totalsum=0;
         int sum=0;
-        for(int i=0;i<n+1;i++){
-            totalsum+=i;
-        }
+        totalsum = n*(n+1)/2;
         for(int i=0;i<n;i++){
             sum+=arr[i];
         }
