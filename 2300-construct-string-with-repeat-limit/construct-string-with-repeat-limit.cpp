2a1448b18fa071646a697a75f83c29b2d1e99617
Ujjawal Patidar <118273885+ujjawalpatidar4@users.noreply.github.com>
2024-12-17 21:50:14 +0530

    Time: 29 ms (72.91%) | Memory: 28.1 MB (32.11%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        sort(s.rbegin(), s.rend());
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        // Step 2: Create a max-heap based on character
+        priority_queue<pair<char, int>> maxHeap;
+        for (char c = 'z'; c >= 'a'; c--) {
+            if (freq[c - 'a'] > 0) {
+                maxHeap.push({c, freq[c - 'a']});
+            }
+        }
 
         string result;
-        int freq = 1;
-        int pointer = 0;
+        
+        // Step 3: Construct the result string
+        while (!maxHeap.empty()) {
+            auto [char1, count1] = maxHeap.top();
+            maxHeap.pop();
 
-        for (int i = 0; i < s.size(); ++i) {
-            if (!result.empty() && result.back() == s[i]) {
-                if (freq < repeatLimit) {
-                    result += s[i];
-                    freq++;
-                } else {
-                    pointer = max(pointer, i + 1);
-                    while (pointer < s.size() && s[pointer] == s[i]) {
-                        pointer++;
-                    }
+            // Determine how many times we can add char1
+            int timesToAdd = min(count1, repeatLimit);
+            result.append(timesToAdd, char1);
+            count1 -= timesToAdd;
 
-                    if (pointer < s.size()) {
-                        result += s[pointer];
-                        swap(s[i], s[pointer]);
-                        freq = 1;
-                    } else {
-                        break;
-                    }
+            // If we still have more of char1 left, we need to add a different character
+            if (count1 > 0) {
+                if (maxHeap.empty()) {
+                    break; // No more characters to use
                 }
-            } else {
-                result += s[i];
-                freq = 1;
+
+                // Get the next largest character
+                auto [char2, count2] = maxHeap.top();
+                maxHeap.pop();
+
+                // Add one instance of char2
+                result += char2;
+                count2--;
+
+                // If char2 still has remaining instances, push it back to the heap
+                if (count2 > 0) {
+                    maxHeap.push({char2, count2});
+                }
+
+                // Push char1 back to the heap with the remaining count
+                maxHeap.push({char1, count1});
             }
         }
 
