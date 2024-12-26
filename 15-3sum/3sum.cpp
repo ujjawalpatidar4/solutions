01f7f51e80a4a431e9d3237fb2fbb534acd904b3
Ujjawal Patidar <118273885+ujjawalpatidar4@users.noreply.github.com>
2024-12-27 00:22:39 +0530

    Time: 119 ms (24.98%) | Memory: 45 MB (20.12%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // Method 1 -> 
+        // Method 1 -> Brute Force
         /* vector<vector<int>> ans;
         int n=nums.size();
         set<vector<int>> check;
@@ -24,14 +24,14 @@ public:
         }
         return ans; */
 
+        // Method 2 -> 1 Loop + 2 Sum Problem(using map)
+        /*
         vector<vector<int>> ans;
         int n=nums.size();
         set<vector<int>> myset;
         for(int i=0;i<n;i++){
             unordered_map<int,int> mpp;
-            // mpp.clear();
             vector<int> ans2;
-
             for(int j=i+1;j<n;j++){
                 int rem=0-nums[i]-nums[j];
                 if(mpp.find(rem) != mpp.end()){
@@ -47,6 +47,41 @@ public:
                     mpp[nums[j]]++;
                 }
                 ans2.clear();
+            }
+        }
+        return ans;
+        */
+
+        // Method 3 -> 1 loop + 2 pointer 
+        vector<vector<int>> ans;
+        int n=nums.size();
+        set<vector<int>> myset;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++){
+            int l=i+1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int r=n-1;
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum<0){
+                    l++;
+                }
+                else if(sum>0){
+                    r--;
+                }
+                else{
+                    vector<int> ans2;
+                    ans2.push_back(nums[i]);
+                    ans2.push_back(nums[l]);
+                    ans2.push_back(nums[r]);
+                    sort(ans2.begin(),ans2.end());
+                    if(myset.find(ans2) == myset.end()){
+                        ans.push_back(ans2);
+                        myset.insert(ans2);
+                    }
+                    l++;
+                    r--;
+                }
             }
         }
         return ans;
