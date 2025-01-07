aea3d41e12ec23b1fbbf8d75e615fb98bc518b78
Ujjawal Patidar <118273885+ujjawalpatidar4@users.noreply.github.com>
2025-01-07 18:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.01%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1;
+        int high=x;
+        int ans=0;
+        if(x==0) return 0;
+        while(low<=high){
+            long long mid=low+(-low+high)/2;
+            long long val=mid*mid;
+            if(val>x){
+                high=mid-1;
+            }else{
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
